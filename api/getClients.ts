8d5064f69b46d65a032e108c47a260c561f322bd
Matias Napoli <mnap@pop-os.localdomain>
2020-08-27 13:11:26 -0300

    Remove mock API server

--- api/getClients.ts
+++ api/getClients.ts
@@ -1,36 +1,5 @@
 import ky from 'ky';
 
-const clients = [
-    { 
-        id: 1, name: "Pepito", age: 28
-    },
-    { 
-        id: 2, name: "Emi", age: 35
-    },
-    { 
-        id: 3, name: "Jorge", age: 46
-    }   
-];
-
-const clientsPromiseMock = new Promise((resolve, reject) => 
-{
-    setTimeout(function () 
-    {
-        let checkId = clients.findIndex((client) => 
-        {
-            return client.id === 99;
-        });
-        if (checkId !== -1)
-        {
-            reject(
-                { 
-                    error: "Invalid value" 
-                });
-        }
-        resolve(clients);
-    }, 10000);
-});
-
 const getClients = () => 
 {
     return ky.get("http://localhost:8000/clients").json();
--- api/getClientsTest.ts
+++
@@ -1,9 +0,0 @@
-import getClients from './getClients';
-
-getClients()
-.then((clients) => {
-    console.log(clients);
-})
-.catch(({error}) => {
-    console.log(error);
-});
