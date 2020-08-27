f688fbf0eb1279091362225de2f959b02bfdc187
Matias Napoli <mnap@pop-os.localdomain>
2020-08-27 18:07:44 -0300

    Remove ActiveCheckbox, and replace it for Checkbox

--- ClientsView.tsx
+++ ClientsView.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, View} from 'react-native';
-import {Button, Text} from 'react-native-paper';
+import {Button, Checkbox, Text} from 'react-native-paper';
 import {useDispatch, useSelector} from 'react-redux';
 import ClientsList from './ClientsList';
-import ActiveCheckbox from './ActiveCheckbox';
 import getClients from './actionCreators';
 
 const styles = StyleSheet.create(
@@ -22,12 +21,22 @@ const styles = StyleSheet.create(
 
 export default function ClientsView()
 {
+    const [checked, setChecked] = React.useState(false);
     const state = useSelector((state) => (state));
     const dispatch = useDispatch();
     
     const showClients = () => {
         dispatch(getClients());
     }
+
+    const setFilter = () => {
+            dispatch(
+                {type: "SET_CLIENTS_FILTER", 
+                filter: (value) => {return value.active;}
+            });
+        setChecked(!checked);
+    }
+
     return (
         <View style={styles.container}>
             <Button onPress={showClients}>Obtener clientes</Button>
@@ -37,7 +46,7 @@ export default function ClientsView()
                 <ClientsList pending={state.pending} clients={state.clients} 
                 filteredClients={state.filteredClients}/>
             }
-            <ActiveCheckbox />
+            <Checkbox status={checked ? "checked" : "unchecked"} onPress={setFilter}/>
             <Text style={styles.text}>ocultar inactivos</Text>
             <StatusBar style="auto" />
         </View>
